8de2ccfd34dd9bc07bc88ecaa731c987fefc17f3
kobihanoch <kobikobi622@gmail.com>
2025-04-26 11:39:28 +0300

    Add Intro, Home, and Login/Register pages with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import Loginandregister from "./pages/Loginandregister.js";
+import Home from "./pages/Authenticated/Home.js";
+import Loginandregister from "./pages/Guests/Loginandregister.js";
+import Intro from "./pages/Guests/Intro.js";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/intro" element={<Loginandregister />} />
+      {/* For guests */}
+      <Route path="/intro" element={<Intro />} />
+      <Route path="/intro/auth" element={<Loginandregister />} />
+
+      {/* For authenticated */}
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
