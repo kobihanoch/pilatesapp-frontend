4b3d940c0eecd19c917f063dd75f43ad7460eb92
kobihanoch <kobikobi622@gmail.com>
2025-04-26 16:23:02 +0300

    Refactor routing logic in App.js to include loading state management for PrivateRoute and GuestRoute

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import Loginandregister from "./pages/Guests/Loginandregister.js";
 import Intro from "./pages/Guests/Intro.js";
 import { useAuthContext } from "./context/authContext.js";
 
+// Route for authenticated users
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuthContext();
-  return user ? children : <Navigate to="/intro" replace />;
+  const { user, loading } = useAuthContext();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return user ? children : <Navigate to="/" replace />;
 };
 
+// Route for guests (not authenticated users)
 const GuestRoute = ({ children }) => {
-  const { user } = useAuthContext();
+  const { user, loading } = useAuthContext();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return !user ? children : <Navigate to="/home" replace />;
 };
 
 function App() {
   return (
+    // Define the routes for the application.
+    // We are protecting the routes using PrivateRoute and GuestRoute components
+    // If a user is authenticated, they will be redirected to the home page if they try to access the login/register page
+    // If a user is not authenticated, they will be redirected to the login/register page if they try to access the home page
     <Routes>
       {/* For guests */}
       <Route
-        path="/intro"
+        path="/"
         element={
           <GuestRoute>
             <Intro />
