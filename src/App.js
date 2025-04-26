7f1c866d4481d83f82fd0d2f86a6fe76f25d244b
kobihanoch <kobikobi622@gmail.com>
2025-04-26 14:08:52 +0300

    Implement route protection with PrivateRoute and GuestRoute components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Authenticated/Home.js";
 import Loginandregister from "./pages/Guests/Loginandregister.js";
 import Intro from "./pages/Guests/Intro.js";
+import { useAuthContext } from "./context/authContext.js";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuthContext();
+  return user ? children : <Navigate to="/intro" replace />;
+};
+
+const GuestRoute = ({ children }) => {
+  const { user } = useAuthContext();
+  return !user ? children : <Navigate to="/home" replace />;
+};
 
 function App() {
   return (
     <Routes>
       {/* For guests */}
-      <Route path="/intro" element={<Intro />} />
-      <Route path="/intro/auth" element={<Loginandregister />} />
+      <Route
+        path="/intro"
+        element={
+          <GuestRoute>
+            <Intro />
+          </GuestRoute>
+        }
+      />
+      <Route
+        path="/intro/auth"
+        element={
+          <GuestRoute>
+            <Loginandregister />
+          </GuestRoute>
+        }
+      />
 
-      {/* For authenticated */}
-      <Route path="/home" element={<Home />} />
+      {/* For authenticated users */}
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
