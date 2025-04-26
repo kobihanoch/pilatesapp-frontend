2bcc9001846e7f90a05b888039fea34d9e9779ff
kobihanoch <kobikobi622@gmail.com>
2025-04-26 11:10:17 +0300

    Add Login and Register page and update routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
+import Loginandregister from "./pages/Loginandregister.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/intro" element={<Loginandregister />} />
     </Routes>
   );
 }
