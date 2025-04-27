cb23a3f5c752033a1f134cea0400d582183ff33f
Lior <l1i9o9r6@gmail.com>
2025-04-27 21:35:20 +0300

    Contact me section is finished

--- src/pages/Guests/Intro.js
+++ src/pages/Guests/Intro.js
@@ -69,7 +69,7 @@ const Intro = () => {
             />
           </div>
           <h1>רותם פילאטיס</h1>
-          <p>description here...</p>
+          {/*<p>description here...</p>*/}
           <button className="get-started-btn" onClick={handleGetStarted}>
             GET STARTED
           </button>
@@ -125,25 +125,62 @@ const Intro = () => {
         </ul>
       </section>
 
-      <section id="?שניצור קשר" className="section">
-        <h2>צור קשר</h2>
-        <p>Phone: 050-123-4567</p>
-        <p>Address: Tel Aviv, Main Street 42</p>
+      <section id="?שניצור קשר" className="section contact-content">
+        <div className="contact-text">
+          <h2>צור קשר</h2>
+          <p>Phone: 050-123-4567</p>
+          <p>Address: Tel Aviv, Main Street 42</p>
 
-        {/* WhatssUP Button*/}
-        <a
-          href="https://wa.me/972504671935"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="whatsapp-button"
-        >
+          {/* Link Logos Button */}
+          <div className="social-links">
+            <a
+              href="https://chat.whatsapp.com/HS1onF48PcR3kmY5pZuw3Q"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/WhatssupIcon.png"
+                alt="WhatsApp Group"
+                className="social-icon"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/pilates_yoga.with.rotem?igsh=MW45cDZxZnYwdHlseA%3D%3D&utm_source=qr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/InstagramIcon.png"
+                alt="Instagram"
+                className="social-icon"
+              />
+            </a>
+          </div>
+
+          {/* WhatsApp Button */}
+          <a
+            href="https://wa.me/972504671935"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp-button"
+          >
+            <img
+              src="/contactMeLogo.png"
+              alt="Phone Icon"
+              className="whatsapp-icon"
+            />
+            דברו איתי בוואטסאפ
+          </a>
+        </div>
+
+        <div className="contact-image">
           <img
-            src="contactMeLogo.png"
-            alt="Phone Icon"
-            className="whatsapp-icon"
+            src="/contactme.jpg"
+            alt="Contact Me"
+            className="background-img"
           />
-          דברו איתי בוואטסאפ
-        </a>
+          <img src="/YogaLogo2.png" alt="Logo" className="corner-logo" />
+        </div>
       </section>
     </div>
   );
