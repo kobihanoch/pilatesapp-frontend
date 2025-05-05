658ec9257a08c10303481d042c50784f66e58c5e
kobihanoch <kobikobi622@gmail.com>
2025-05-05 19:06:40 +0300

    Remove console log from unregister session function

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,9 +1,20 @@
 import api from "../api/api";
 
+// Unregister a session
 export const unregisterFromSelectedSession = async (sessionId) => {
   try {
-    console.log(sessionId);
     const response = await api.post(`/api/sessions/unregister/${sessionId}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error.response.data.message;
+  }
+};
+
+// Register a session
+export const registerToSelectedSession = async (sessionId) => {
+  try {
+    const response = await api.post(`/api/sessions/register/${sessionId}`);
     return response.data;
   } catch (error) {
     console.log(error);
