d81d858a758a77f879df3001b5b7f092892b95c4
kobihanoch <kobikobi622@gmail.com>
2025-05-10 14:50:40 +0300

    Added date selection for sessions

--- src/components/HomeComponents/SignUpSectionListComponents/AvailableSessionItem.js
+++ src/components/HomeComponents/SignUpSectionListComponents/AvailableSessionItem.js
@@ -3,8 +3,18 @@ import { formatDate } from "../../../utils/homeUtils";
 
 const AvailableSessionItem = ({ session }) => {
   return (
-    <div style={{ width: "100%", height: 10 }}>
-      <h1>Content</h1>
+    <div
+      style={{
+        width: "100%",
+        height: "150px",
+        marginBottom: "12px",
+        borderRadius: "12px",
+        backgroundColor: "#f9f9f9",
+        boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+      }}
+    >
+      <p>{formatDate(session.date)}</p>
+      <p>{session.time}</p>
     </div>
   );
 };
--- src/components/HomeComponents/SignupSection.js
+++ src/components/HomeComponents/SignupSection.js
@@ -2,19 +2,71 @@ import React, { useState, useEffect } from "react";
 import AvailableSessionItem from "./SignUpSectionListComponents/AvailableSessionItem";
 
 const SignupSection = ({ availableSessions }) => {
-  useEffect(() => {
-    if (availableSessions) console.log(availableSessions);
-  }, [availableSessions]);
+  const [selectedDate, setSelectedDate] = useState(() => {
+    const today = new Date();
+    return today.toISOString().split("T")[0];
+  });
+
   return (
-    <div style={{ backgroundColor: "green" }}>
-      {availableSessions?.map((session) => (
-        <AvailableSessionItem
-          key={session._id}
-          session={session}
-        ></AvailableSessionItem>
-      ))}
+    <div
+      style={{
+        borderColor: "black",
+        alignSelf: "center",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        height: "80vh",
+        paddingBottom: "100px",
+      }}
+    >
+      <h3 style={styles.sectionTitle}>אימונים זמינים להרשמה</h3>
+      <span
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 20,
+          alignSelf: "center",
+        }}
+      >
+        <p>תאריך רצוי</p>
+        <input
+          type="date"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}
+        ></input>
+      </span>
+      <div
+        style={{
+          overflowY: "scroll",
+          display: "flex",
+          width: "90%",
+          alignSelf: "center",
+          flex: 9,
+          flexDirection: "column",
+          marginTop: "30px",
+        }}
+      >
+        {availableSessions?.map((session) => (
+          <AvailableSessionItem
+            key={session._id}
+            session={session}
+          ></AvailableSessionItem>
+        ))}
+      </div>
     </div>
   );
+};
+
+const styles = {
+  sectionTitle: {
+    fontSize: 25,
+    color: "black",
+    marginBottom: 12,
+    marginTop: 20,
+    paddingRight: 10,
+    flex: 1,
+    marginTop: "40px",
+  },
 };
 
 export default SignupSection;
