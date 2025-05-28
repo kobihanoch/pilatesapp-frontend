c4a27ce32c5afa9aaea6f99cb8a4c1e1a3c70e85
kobihanoch <kobikobi622@gmail.com>
2025-05-28 19:37:10 +0300

    fix(ErrorProvider): Change toast notification from error to warning for error messages

--- src/context/errorContext.js
+++ src/context/errorContext.js
@@ -24,7 +24,7 @@ export const ErrorProvider = ({ children }) => {
   // Toasting auto when new error comes in
   useEffect(() => {
     if (error) {
-      toast.error(
+      toast.warn(
         `שגיאה${error.status ? " " + error.status : ""}: ${error.message}`
       );
     }
