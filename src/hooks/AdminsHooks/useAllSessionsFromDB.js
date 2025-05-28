a658415dce3e6ad02b7b28d500fd34c5db89d3bc
kobihanoch <kobikobi622@gmail.com>
2025-05-28 23:14:14 +0300

    fix(useAllSessionsFromDB): Ensure loading state is managed correctly during data fetch

--- src/hooks/AdminsHooks/useAllSessionsFromDB.js
+++ src/hooks/AdminsHooks/useAllSessionsFromDB.js
@@ -5,15 +5,19 @@ import { useErrorContext } from "../../context/errorContext.js";
 // ADMINS
 const useAllSessionsFromDB = () => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const { setError } = useErrorContext();
 
   useEffect(() => {
     (async () => {
       try {
+        setLoading(true);
         const res = await fetchAllSessions();
         setData(res);
       } catch (e) {
         setError(e);
+      } finally {
+        setLoading(false);
       }
     })();
   }, []);
