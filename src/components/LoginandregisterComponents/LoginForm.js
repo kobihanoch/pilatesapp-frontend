62394a388ac80ffbeb0b4a78042e19bc66d6ec82
kobihanoch <kobikobi622@gmail.com>
2025-06-04 07:05:18 +0300

    refactor(LoginForm): Implement login validation utility function

--- src/components/LoginandregisterComponents/LoginForm.js
+++ src/components/LoginandregisterComponents/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAuthContext } from "../../context/authContext";
 import { FiUser, FiLock } from "react-icons/fi";
 import { useErrorContext } from "../../context/errorContext";
+import { validateLogin } from "../../utils/loginUtils";
 
 const LoginForm = () => {
   const { auth } = useAuthContext();
@@ -15,17 +16,9 @@ const LoginForm = () => {
     }));
   };
 
-  const validateLogin = () => {
-    if (!formData.username || !formData.password) {
-      setError(new Error("חובה למלא שם משתמש וסיסמה"));
-      return false;
-    }
-    return true;
-  };
-
   const handleLogin = async () => {
-    if (!validateLogin()) return;
     try {
+      validateLogin(formData.username, formData.password); // Throws expection if form is not fully filled
       await auth.login(formData.username, formData.password);
       console.log("Login successful!");
     } catch (error) {
