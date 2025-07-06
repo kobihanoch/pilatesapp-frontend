b93508284dbf0daf508b04d3b364e7dcd1b136c4
kobihanoch <kobikobi622@gmail.com>
2025-07-06 22:49:49 +0300

    fix(SharedUtils): Fixed filtering for week to a week starts from sunday to saturday night (Israel time)

--- src/utils/sharedUtils.js
+++ src/utils/sharedUtils.js
@@ -14,16 +14,34 @@ export const validateForm = (formData) => {
 };
 
 export const filterRegisteredSessionToThisWeekSessions = (sessions) => {
-  const thisWeek = getISOWeek(new Date());
-  const thisYear = getYear(new Date());
+  console.log("All upcoming: ", sessions);
+
+  // Get today's dat
+  const today = new Date();
+  // Set sunday from 00:00:00:00
+  const thisWeekSundayMorning = new Date(); // Returns sunday (0)
+  thisWeekSundayMorning.setDate(today.getDate() - today.getDay()); // Returns sunday (0)
+  thisWeekSundayMorning.setHours(0, 0, 0, 0);
+
+  console.log("Sunday: ", thisWeekSundayMorning);
+
+  // Set saturday until 23:59:59:999
+  const thisWeekSaturdayNight = new Date();
+  thisWeekSaturdayNight.setDate(thisWeekSundayMorning.getDate() + 6);
+  thisWeekSaturdayNight.setHours(23, 59, 59, 999);
+
+  console.log("Saturday: ", thisWeekSaturdayNight);
 
   // Filter sessions to only include those from the current week
   let filteredSessions = sessions.filter((s) => {
-    const sessionWeek = getISOWeek(new Date(s.date));
-    const sessionYear = getYear(new Date(s.date));
-    return sessionWeek === thisWeek && sessionYear === thisYear;
+    const sessionDate = new Date(s.date);
+    return (
+      sessionDate >= thisWeekSundayMorning &&
+      sessionDate <= thisWeekSaturdayNight
+    );
   });
 
+  // Filter only cancled sessions
   let cancledCount = filteredSessions.filter((s) => s.status === "בוטל").length;
 
   // Sort from sooner to latest
