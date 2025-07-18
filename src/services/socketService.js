3d5e61406e721e0b3fe039d5041474f46b9151a0
kobihanoch <kobikobi622@gmail.com>
2025-07-18 21:51:07 +0300

    fix(Socket): Correct event name from "user_connected" to "user_loggedin"

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -2,7 +2,7 @@ import { socket } from "../socketConfig/socketConfig";
 
 export const createFirstSocketConnection = async (userId) => {
   socket.connect();
-  socket.emit("user_connected", userId);
+  socket.emit("user_loggedin", userId);
 };
 
 export const disconnectSocket = async () => {
